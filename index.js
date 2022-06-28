17ddca279e414b04ba7cd46c3fd7f54fa42a9ec7
Kaif.JU.CSE <90242848+kaifsardar@users.noreply.github.com>
2022-06-28 16:07:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-let roll;
+
     function submit() {
+        let roll;
    roll=document.getElementById("roll").value; 
    document.querySelector("#img1").setAttribute("src", "http://juadmission.jdvu.ac.in/jums_exam/student_photos/002110501" + roll + ".jpg");
    document.querySelector("#img2").setAttribute("src", "http://juadmission.jdvu.ac.in/jums_exam/student_photos/002110501" + roll + ".jpeg");
