4c10e548b78ab85dcd7f13eab36a49aac6b9d115
Kaif.JU.CSE <90242848+kaifsardar@users.noreply.github.com>
2022-06-30 21:26:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 
-  function submit() {
-   roll=document.getElementById("roll").value; 
-   document.querySelector("#img1").setAttribute("src", "http://juadmission.jdvu.ac.in/jums_exam/student_photos/002110501" + roll + ".jpg");
-   document.querySelector("#img2").setAttribute("src", "http://juadmission.jdvu.ac.in/jums_exam/student_photos/002110501" + roll + ".jpeg");
+let obj = {
+  "1": "002110501",
+  "2": "002110701",
+  "3": "002110601",
+  "4": "002110801",
+  "5": "002111201",
+  "6": " ",
+  "7": "002110301",
+  "8": "002110901",
+  "9": "002111701",
+
+  
+ 
+ 
+}
+
+function submit() {
+
+    let roll1=obj[document.getElementById("dept").value];
+    let roll2=document.getElementById("roll").value; 
+   document.querySelector("#img1").setAttribute("src", "http://juadmission.jdvu.ac.in/jums_exam/student_photos/"+roll1+roll2+ ".jpg");
+   document.querySelector("#img2").setAttribute("src", "http://juadmission.jdvu.ac.in/jums_exam/student_photos/"+roll1 +roll2+ ".jpeg");
+
    
 }
 
